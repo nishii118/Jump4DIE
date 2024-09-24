f04d59afc96b450acbd88dddc31b32b29fd60bc2
PhucDev02 <phuc12610@gmail.com>
2024-09-24 08:15:02 +0700

    update some audio scripts

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -12,15 +12,28 @@ public class AudioManager : Singleton<AudioManager>
     {
         PlayMusic("Background");
     }
-
+    public static bool GetToggleState(string key)
+    {
+        return PlayerPrefs.GetInt(key, 1) == 1 ? true : false;
+    }
+    public static void ToggleState(string key)
+    {
+        PlayerPrefs.SetInt(key, PlayerPrefs.GetInt(key, 1));
+        if(key=="Music")
+        {
+            //TODO: stop or play Music (background)
+        }
+    }
     public void PlayMusic(string name)
     {
+        if (!GetToggleState("Music")) return;
         Sound s = Array.Find(musicSounds, x => x.name == name);
 
         if (s == null)
         {
             Debug.Log("Sound not found");
-        } else
+        }
+        else
         {
             musicSource.clip = s.clip;
             musicSource.loop = true;
@@ -29,21 +42,27 @@ public class AudioManager : Singleton<AudioManager>
 
         }
     }
-    
+
     public void PlaySFX(string name)
     {
+        if (!GetToggleState("Sound")) return;
         Sound s = Array.Find(sfxSounds, x => x.name == name);
 
         if (s == null)
         {
             Debug.Log("Sound not found");
-        } else
+        }
+        else
         {
             sfxSource.clip = s.clip;
             sfxSource.Play();
         }
     }
-
+    public static void Vibrate()
+    {
+        if (!GetToggleState("Vibration")) return;
+        Handheld.Vibrate();
+    }
 
     public void ToggleMusic()
     {
@@ -56,7 +75,7 @@ public class AudioManager : Singleton<AudioManager>
         sfxSource.mute = !sfxSource.mute;
     }
 
-    
 
-    
+
+
 }
